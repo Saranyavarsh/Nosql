d235d974ddfa376ce015f7034233f2aead42cde1
Saranyavarsh <124798973+Saranyavarsh@users.noreply.github.com>
2023-05-24 23:08:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,4 +25,38 @@ app.get('/', async (req, res) => {
     edit_stationaryproduct = await collection.findOne({ _id:new ObjectID(edit_id) })
   }
   
+  if (req.query.delete_id) {
+    await collection.deleteOne({_id:ObjectID(req.query.delete_id)})
+    return res.redirect('/?status=3');
+  }
+  
+  switch (req.query.status) {
+      case '1':
+       message = 'Inserted Succesfully!';
+       break;
+      
+      case '2':
+       message = 'Updated Succesfully!';
+       break;
+      
+      case '3':
+       message = 'Deleted Succesfully!';
+       break;
+      
+      default:
+       break;
+    }
+  
+  
+  res.render('main',{message, stationaryproducts,edit_id,edit_stationaryproduct})
+})
+
+app.post('/store_stationaryproduct',async (req,res)=>{
+  let database = await dbo.getDatabase();
+  const collection = database.collection('stationaryproducts');
+  let stationaryproduct= { name: req.body.name, quantity: req.body.quantity,price:req.body.price  };
+  await collection.insertOne(stationaryproduct);
+  return res.redirect('/?status=1');
+})
+
   
